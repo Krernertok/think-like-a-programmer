8ceaa0a0a697af096b664740619732059a2e012a
Antti Sivula <antti.j.sivula@gmail.com>
2018-11-19 22:50:50 +0200

    Add solution for exercise 02 of chapter 2

---
+++ chapter_2/exercise_2-02.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Produce the following output with nothing but the hash mark (#) and a newline character (\n):
+//
+//    ##
+//   ####
+//  ######
+// ########
+// ########
+//  ######
+//   ####
+//    ##
+func main() {
+	lineLength := 8
+	halfLine := lineLength / 2
+
+	for i := 1; i <= halfLine; i++ {
+		for j := 0; j < halfLine-i; j++ {
+			fmt.Print(" ")
+		}
+		for k := 0; k < (2 * i); k++ {
+			fmt.Print("#")
+		}
+		fmt.Print("\n")
+	}
+
+	for i := 0; i < halfLine; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Print(" ")
+		}
+		for k := 0; k < (lineLength - (2 * i)); k++ {
+			fmt.Print("#")
+		}
+		fmt.Print("\n")
+	}
+}

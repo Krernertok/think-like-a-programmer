efe2b9482e08311c9167443aa473eecacc1c0b1a
Antti Sivula <antti.j.sivula@gmail.com>
2018-11-19 23:46:46 +0200

    Add solution for exercise 03 of chapter 2

---
+++ chapter_2/exercise_2-03.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Produce a custom output with nothing but the hash mark (#) and a newline character (\n):
+//
+//   ###    ###
+//    ##    ##
+//    ##   ##
+//    ##  ##
+//    ## ##
+//    ##   ##
+//    ##    ##
+//    ##     ##
+//   ###     ###
+
+func main() {
+
+}

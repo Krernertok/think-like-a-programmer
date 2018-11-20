31fd0819d4d27c19d4042ef89672e2c8e64f2f0a
Antti Sivula <antti.j.sivula@gmail.com>
2018-11-20 22:56:04 +0200

    Add half a solution for exercise 03 in chapter 2

--- chapter_2/exercise_2-03.go
+++ chapter_2/exercise_2-03.go
@@ -2,18 +2,35 @@ package main
 
 import "fmt"
 
-// Produce a custom output with nothing but the hash mark (#) and a newline character (\n):
+// Produce a custom output with nothing but the hash mark (#), a space ( ) and a newline character (\n):
 //
+// #            #
+//  ##        ##
 //   ###    ###
-//    ##    ##
-//    ##   ##
-//    ##  ##
-//    ## ##
-//    ##   ##
-//    ##    ##
-//    ##     ##
-//   ###     ###
+//    ########
+//    ########
+//   ###    ###
+//  ##        ##
+// #            #
 
 func main() {
+	shapeWidth := 14
+	shapeHeight := 8
 
+	for i := 1; i <= shapeHeight/2; i++ {
+		var j, k int
+		for j = 0; j < i-1; j++ {
+			fmt.Print(" ")
+		}
+		for k = 0; k < i; k++ {
+			fmt.Print("#")
+		}
+		for l := 0; l < (shapeWidth - 2*j - 2*k); l++ {
+			fmt.Print(" ")
+		}
+		for m := 1; m <= i; m++ {
+			fmt.Print("#")
+		}
+		fmt.Print("\n")
+	}
 }

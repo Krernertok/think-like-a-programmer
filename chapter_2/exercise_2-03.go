7b31c9a60d7f0cb6234e78352b74e065dcf92934
Antti Sivula <antti.j.sivula@gmail.com>
2018-11-20 23:12:27 +0200

    Update full solution for exercise 03 of chapter 2

--- chapter_2/exercise_2-03.go
+++ chapter_2/exercise_2-03.go
@@ -18,6 +18,7 @@ func main() {
 	shapeWidth := 14
 	shapeHeight := 8
 
+	// upper half of the shape
 	for i := 1; i <= shapeHeight/2; i++ {
 		var j, k int
 		for j = 0; j < i-1; j++ {
@@ -29,7 +30,25 @@ func main() {
 		for l := 0; l < (shapeWidth - 2*j - 2*k); l++ {
 			fmt.Print(" ")
 		}
-		for m := 1; m <= i; m++ {
+		for m := 0; m < i; m++ {
+			fmt.Print("#")
+		}
+		fmt.Print("\n")
+	}
+
+	// lower half of the shape
+	for i := shapeHeight / 2; i > 0; i-- {
+		var j, k int
+		for j = 0; j < i-1; j++ {
+			fmt.Print(" ")
+		}
+		for k = 0; k < i; k++ {
+			fmt.Print("#")
+		}
+		for l := 0; l < (shapeWidth - 2*j - 2*k); l++ {
+			fmt.Print(" ")
+		}
+		for m := 0; m < i; m++ {
 			fmt.Print("#")
 		}
 		fmt.Print("\n")

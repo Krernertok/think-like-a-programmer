232c9b7de5ce818ccdf0bd3a1bd57da89c8c7904
Antti Sivula <antti.j.sivula@gmail.com>
2018-12-06 19:09:24 +0200

    Add linked list to solution

--- chapter_4/exercise_4-01.go
+++ chapter_4/exercise_4-01.go
@@ -18,6 +18,44 @@ func (a *arrayString) concatenate(b arrayString) {
 	*a = append(*a, b...)
 }
 
+type listNode struct {
+	studentID int
+	grade     int
+	next      *listNode
+}
+
+type studentCollection struct {
+	headPointer *listNode
+}
+
+func (sc *studentCollection) addRecord(id, grade int) {
+	newNode := listNode{
+		studentID: id,
+		grade:     grade,
+		next:      sc.headPointer,
+	}
+	sc.headPointer = &newNode
+}
+
+func (sc studentCollection) averageRecord() int {
+	node := sc.headPointer
+
+	if node == nil {
+		return 0
+	}
+
+	sum := 0
+	nodeCount := 0
+
+	for node != nil {
+		sum += node.grade
+		nodeCount++
+		node = node.next
+	}
+
+	return sum / nodeCount
+}
+
 func main() {
 	a := arrayString{'h', 'e', 'l', 'l', 'o'}
 	b := arrayString{'w', 'o', 'r', 'l', 'd'}
@@ -32,4 +70,17 @@ func main() {
 	fmt.Println(string(a))
 
 	fmt.Println(string(a.runeAt(0)))
+
+	fmt.Println("---")
+
+	node := listNode{1, 80, nil}
+	sc := studentCollection{&node}
+	sc.addRecord(2, 90)
+	sc.addRecord(3, 100)
+	sc.addRecord(4, 100)
+
+	fmt.Println("Average:", sc.averageRecord())
+
+	empty := studentCollection{nil}
+	fmt.Println("Average of empty:", empty.averageRecord())
 }

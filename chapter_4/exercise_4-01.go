df195701ae9def8159209fe1440684aff42e994f
Antti Sivula <antti.j.sivula@gmail.com>
2018-12-06 18:32:30 +0200

    Add solution for exercise 1 of chapter 4

--- chapter_4/exercise_4-01.go
+++ chapter_4/exercise_4-01.go
@@ -1,7 +1,35 @@
+/*Since slices are dynamical, I will implement the string and linked
+list structures instead.*/
 package main
 
 import "fmt"
 
-func main() {
+type arrayString []rune
 
+func (a *arrayString) append(r rune) {
+	*a = append(*a, r)
+}
+
+func (a arrayString) runeAt(index int) rune {
+	return a[index]
+}
+
+func (a *arrayString) concatenate(b arrayString) {
+	*a = append(*a, b...)
+}
+
+func main() {
+	a := arrayString{'h', 'e', 'l', 'l', 'o'}
+	b := arrayString{'w', 'o', 'r', 'l', 'd'}
+
+	fmt.Println(string(a), string(b))
+
+	a.append(',')
+	a.append(' ')
+	fmt.Println(string(a))
+
+	a.concatenate(b)
+	fmt.Println(string(a))
+
+	fmt.Println(string(a.runeAt(0)))
 }

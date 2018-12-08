9a9d130e9f7fe76fe00db295e9e51dae93a2db5a
Antti Sivula <antti.j.sivula@gmail.com>
2018-12-08 11:57:29 +0200

    Add solution for exercise 9 of chapter 4

---
+++ chapter_4/exercise_4-09.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type intNode struct {
+	value int
+	next  *intNode
+}
+
+type intList struct {
+	firstNode *intNode
+}
+
+func intToList(num int) *intList {
+	var node *intNode
+	var iList intList
+
+	for num > 0 {
+		mod := num % 10
+		n := intNode{
+			value: mod,
+			next:  nil,
+		}
+
+		if node == nil {
+			node = &n
+		} else {
+			n.next = node
+			node = &n
+		}
+
+		num /= 10
+	}
+
+	iList = intList{node}
+
+	return &iList
+}
+
+func main() {
+	integer := intToList(123)
+
+	node := integer.firstNode
+
+	for node != nil {
+		fmt.Println(node.value)
+		node = node.next
+	}
+}

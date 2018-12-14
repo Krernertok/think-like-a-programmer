e2c58bf85deeb95c159f7a54ce272cbf17b3ce54
Antti Sivula <antti.j.sivula@gmail.com>
2018-12-14 14:50:10 +0200

    Add solution for exercise 3 of chapter 7

---
+++ chapter_7/exercise_7-03.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type student struct {
+	grade int
+	id    int
+	name  string
+}
+
+type byId []student
+
+func printStudents(students []student) {
+	for i, s := range students {
+		fmt.Printf("%d: %s (ID: %d) - %d\n", i+1, s.name, s.id, s.grade)
+	}
+}
+
+func (b byId) Len() int {
+	return len(b)
+}
+
+func (b byId) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+
+func (b byId) Less(i, j int) bool {
+	return b[i].id < b[j].id
+}
+
+func searchByID(s []student, id int) (student, bool) {
+	lo := 0
+	hi := len(s) - 1
+
+	for lo != hi && id >= s[lo].id && id <= s[hi].id {
+		mid := lo + ((id - s[lo].id) * (hi - lo) / (s[hi].id - s[lo].id))
+
+		if s[mid].id < id {
+			lo = mid + 1
+		} else if s[mid].id > id {
+			hi = mid - 1
+		} else {
+			return s[mid], true
+		}
+	}
+
+	if s[lo].id == id {
+		return s[lo], true
+	}
+
+	return student{}, false
+}
+
+func main() {
+	students := []student{
+		{87, 10001, "Fred"},
+		{28, 10002, "Tom"},
+		{100, 10003, "Alistair"},
+		{78, 10004, "Sasha"},
+		{84, 10005, "Erin"},
+		{98, 10006, "Belinda"},
+		{75, 10007, "Leslie"},
+		{70, 10008, "Candy"},
+		{81, 10009, "Aretha"},
+		{68, 10010, "Veronica"},
+	}
+
+	sort.Sort(byId(students))
+	printStudents(students)
+
+	ids := []int{10001, 10010, 10003, 10009, 1000000}
+
+	for _, id := range ids {
+		fmt.Println("---")
+		fmt.Println("Searching for ID:", id)
+		student, ok := searchByID(students, id)
+
+		if ok {
+			fmt.Printf("Found student named %s with ID %d\n", student.name, student.id)
+		} else {
+			fmt.Println("Could not find student with ID:", id)
+		}
+	}
+
+}
